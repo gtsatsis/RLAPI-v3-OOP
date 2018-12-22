2fd93ff4b23a5e452b7c31015518b26f5d9dcb15
SamuelSimao <mucapapipa.br@gmail.com>
2018-12-22 12:20:18 -0200

    unfinished class skeleton

--- src/User.php
+++ src/User.php
@@ -6,5 +6,14 @@
 
 class User
 {
+  public $username;
+  public $token;
+  public $email;
+  public $tier;
+  public $isLoggedIn;
+  // TODO: add all variables
+  
+  public function __construct($username, $password = null) {} 
+  public funcion setTier($tier) {}
 
 }
