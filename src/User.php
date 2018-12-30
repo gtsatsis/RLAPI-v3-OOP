ae264574797ac00caf9244d78e0e0b2d2ae870c5
George Tsatsis <gtsatsis@staff.ratelimited.me>
2018-12-30 18:56:25 +0200

    Initiate a few more functions

--- src/User.php
+++ src/User.php
@@ -16,17 +16,13 @@ class User
   public $isLoggedIn;
   public $isAdmin;
   public $isBlocked;
-  private $preparedStatement;
-
   // TODO: add all variables
   
   public function __construct($username, $password = null) {
     require '../inc/development_db_password.inc.php';
     $dbconn = pg_connect("host=localhost port=5432 dbname=rlapi_devel user=rlapi_devel password=".$dbPass);
   } 
-  public function setTier($userid, $tier) {
-	
-  }
+
   public function createUser($username, $password, $email){
     // First sanitize user input
     $this->username = htmlspecialchars($username);
@@ -44,6 +40,18 @@ class User
 
     $this->preparedStatement = pg_prepare($dbconn, "create_user", "INSERT INTO users ('id', 'username', 'password', 'email', 'tier', 'is_admin', 'is_blocked') VALUES ($1, $2, $3, $4, 'free', false, false)");
     $this->executePreparedStatement =  pg_execute($dbconn, "create_user", array($this->userid, $this->username, $this->password, $this->email));
+  }
+
+  public function createUserAPIKey($id, $email) {
+
+  }
+
+  public function deleteUserAPIKey($apikey, $id, $email) {
+    
+  }
+
+  public function setUserTier($email) {
+
   }
 
 }
