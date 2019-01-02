ff2d6d51485326c0fd62cf5333a8ae05995b2316
Sxribe <barnabas.05.kiss@gmail.com>
2019-01-02 10:41:14 -0500

    Rewrite Uploader.php

--- src/Uploader.php
+++ src/Uploader.php
@@ -1,22 +1,26 @@
 <?php
 
-/**
- * Class
- */
-
 class Uploader
 {
   public $bucket;
   public $filename;
 
-  public function __construct($bucket) {
-    if(array_key_exists($_GET, 'bucket') && !is_null($_GET['bucket']){
-	$this->bucket = $_GET['bucket'];
-    }else{
-	$this->bucket = 'owoapi';
+  public function __construct($bucket)
+  {
+    if(isset($_GET['bucket']) && !is_null($_GET['bucket'])) //Switch from array_key_exists to isset
+    {
+      $this->bucket = 'owoapi';
+    }
+    else
+    {
+      die("\$_GET[\"bucket\"] is not set correctly");
+    }
   }
-  
-  public function upload($apikey) {
-	
+
+  public function upload($apikey)
+  {
+    //TODO: write code to upload file with an apikey
   }
 }
+
+?>
