230697df1d62701b67008bf61cf6f9906913fa28
Sxribe <38634890+Sxribe@users.noreply.github.com>
2019-01-07 09:45:15 -0500

    Add errorfinding for add_domain

--- src/Domains/DomainController.php
+++ src/Domains/DomainController.php
@@ -25,16 +25,36 @@ class DomainController
     $this->sentry_instance = new SentryInstance();
   }
 
-  public function addDomain($id, $domainname){
-      $dr = new DomainRequest;
+    public function addDomain($id, $domainname){
+        $dr = new DomainRequest;
 
-      $domainname = htmlspecialchars($domainname);
-      $validationHash = md5($domainname);
+        $domainname = htmlspecialchars($domainname);
+        $validationHash = md5($domainname);
 
-      $expiryDate = $dr->getExpirationDate($domainname);
-          
-      $prepareStatement = pg_prepare($dbconn, "add_domain", "INSERT INTO domains ('id', 'user_id', 'domainname', 'validated', 'validationhash', 'official', 'type', 'bucket', 'expirydate') VALUES ($1, $2, $3, false, $4, false, 'public', 'owoapi', $5)");
-      $executePreparedStatement = pg_execute($dbconn, "add_domain", array($domainid, $userid, $domainname, $validationHash, $expiryDate));
+        $expiryDate = $dr->getExpirationDate($domainname);
+            
+        $prepareStatement = pg_prepare($dbconn, "add_domain", "INSERT INTO domains ('id', 'user_id', 'domainname', 'validated', 'validationhash', 'official', 'type', 'bucket', 'expirydate') VALUES ($1, $2, $3, false, $4, false, 'public', 'owoapi', $5)");
+        $executePreparedStatement = pg_execute($dbconn, "add_domain", array($domainid, $userid, $domainname, $validationHash, $expiryDate));
+        if($prepareStatement !== false && $executePreparedStatement !== false)
+        {
+          return
+            [
+              'success' => true,
+              'domain' => [
+                'added' => true
+              ]
+            ];
+        }
+        else
+        {
+          return
+            [
+              'success' => false,
+              'error_code' => 302882
+            ];
+          $this->sentry_instance->log_error('There was a domain addition oopsie. Check logs (ln 55) Time: ' . gmdate("Y-m-d H:i:s", time()));
+        }
     }
 
 }
+
