b883efb88dc7cbef83db06ecb7a76a6a2c8d74ce
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-01-31 14:28:04 +0200

    Add the change bucket function
    This adds in a way to change the bucket of a domain, but also adds a proper domain ID on domain creation

--- src/Domains/DomainController.php
+++ src/Domains/DomainController.php
@@ -25,13 +25,15 @@ class DomainController
     $this->sentry_instance = new SentryInstance();
   }
 
-    public function addDomain($id, $domainname){
+    public function addDomain($userid, $domainname){
         $dr = new DomainRequest;
 
-        $domainname = htmlspecialchars($domainname);
+        $domainname = $domainname;
         $validationHash = md5($domainname);
 
         $expiryDate = $dr->getExpirationDate($domainname);
+
+        $domainid = Uuid::uuid4()->toString();
             
         $prepareStatement = pg_prepare($dbconn, "add_domain", "INSERT INTO domains ('id', 'user_id', 'domainname', 'validated', 'validationhash', 'official', 'type', 'bucket', 'expirydate') VALUES ($1, $2, $3, false, $4, false, 'public', 'owoapi', $5)");
         $executePreparedStatement = pg_execute($dbconn, "add_domain", array($domainid, $userid, $domainname, $validationHash, $expiryDate));
@@ -78,8 +80,30 @@ class DomainController
                 'success' => false,
                 'error_code' => 302882
             ];
-            $this->sentry_instance->log_error('There was a domain addition oopsie. Check logs (ln 81) Time: ' . gmdate("Y-m-d H:i:s", time()));
+            $this->sentry_instance->log_error('There was a domain removal oopsie. Check logs (ln 83) Time: ' . gmdate("Y-m-d H:i:s", time()));
         }
     }
+
+    public function setDomainBucket($domainid, $bucket){
+      $prepareStatement = pg_prepare($dbconn, "change_domain_bucket", "UPDATE domains SET bucket = $1 WHERE id = $2");
+      $executePreparedStatement = pg_execute($dbconn, "change_domain_bucket", array($bucket, $domainid));
+
+      if($prepareStatement && $executePreparedStatement){
+        return
+            [
+                'success' => true,
+                'domain' => [
+                    'bucket' => $bucket
+                ]
+            ];
+      }else{
+        return
+            [
+                'success' => false,
+                'error_code' => 302882
+            ];
+            $this->sentry_instance->log_error('Error upon bucket change. Time:' . gmdate("Y-m-d H:i:s", time()));
+      }
+    } 
 
 }
