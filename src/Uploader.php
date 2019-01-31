61d9032471806f2814b56270e4dc9ce38fba0e35
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-01-31 19:32:24 +0200

    Uploader rewrite
    This is a rewrite of the Uploader function. It is not even close to being done, and currently only offers 2 error codes and nothing else.
    
    More to be written soon, but first, I need some coffee

--- src/Uploader.php
+++ src/Uploader.php
@@ -1,26 +1,48 @@
 <?php
 namespace RLME;
 
+include_once '../../vendor/autoload.php';
+
+use RLME\Utils\Sentry;
+use RLME\Models\User;
+use RLME\Models\Apikeys;
+
 class Uploader
 {
   public $bucket;
   public $filename;
+  public $sentry_instance;
 
-  public function __construct($bucket)
+  public function __construct($bucket='owoapi')
   {
-    if(isset($_GET['bucket']) && !is_null($_GET['bucket'])) //Switch from array_key_exists to isset
-    {
-      $this->bucket = 'owoapi';
-    }
-    else
-    {
-      die("\$_GET[\"bucket\"] is not set correctly");
-    }
+    include '../inc/development_db_password.inc.php';
+    $this->sentry_instance = new SentryInstance();
   }
 
-  public function upload($apikey)
+  public function upload($apikey, $files)
   {
-    //TODO: write code to upload file with an apikey
+
+    if(empty($apikey)){
+      return [
+        'success' => false,
+        'error' => [
+          'error_code' => 10000,
+          'error_message' => 'You have not included an API key in your request.'
+        ]
+      ];
+    }
+
+    if(empty($files)){ // If there are no files uploaded, give a generic error.
+      return [
+        'success' => false,
+        'error' => [
+          'error_code' => 10001,
+          'error_message' => 'Your POST request did not include a file in the files[] parameter'
+        ]
+      ];
+    }
+
+    
   }
 }
 
