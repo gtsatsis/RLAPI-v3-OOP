510d2f9c73f9054fc5b7077c2e38b0abc9e733cc
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-04-02 10:56:06 +0300

    Mailer: add reset case

--- src/Utils/Mailer.php
+++ src/Utils/Mailer.php
@@ -47,6 +47,39 @@ class Mailer {
 		$mailer->send($message);
 
 	}
+
+
+	public function send_password_reset_email(string $user_email, $reset_id){
+
+		$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../../templates/');
+		$twig = new \Twig\Environment($loader, [
+				'cache' => __DIR__.'/../../templates_c/',
+		]);
+
+		$transport = (new \Swift_SmtpTransport(getenv('SMTP_SERVER'), getenv('SMTP_PORT'), 'tls'))
+				->setUsername(getenv('SMTP_USERNAME'))
+				->setPassword(getenv('SMTP_PASSWORD'));
+
+
+		$mailer = new \Swift_Mailer($transport);
+
+		$message = (new \Swift_Message('Request to reset your password on RATELIMITED'))
+				->setFrom(getenv('SUPPORT_EMAIL'))
+				->setTo($user_email)
+				->setSubject('Request to reset your password on RATELIMITED')
+				->setBody(
+						$twig->render(
+								'emails/reset_password.html.twig',
+								[
+									'reset_id' => $reset_id
+								]
+						),
+						'text/html');
+
+		$mailer->send($message);
+
+	}
+
 }
 
 ?>
