694c7ca17d17b9d464c84b87810091ad432f40db
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-04-03 09:46:58 +0300

    whoops.. fix reset_id to true because im a dumb

--- src/Models/User.php
+++ src/Models/User.php
@@ -427,8 +427,8 @@ class User {
 
 					if($reset_password['success'] == true){
 
-						pg_prepare($this->dbconn, "reset_password_set_used", "UPDATE password_resets SET id = $1 AND used IS NOT true");
-						$execute_prepared_statement = pg_execute($this->dbconn, "reset_password_set_used", array($reset_id));
+						pg_prepare($this->dbconn, "reset_password_set_used", "UPDATE password_resets SET id = $1");
+						$execute_prepared_statement = pg_execute($this->dbconn, "reset_password_set_used", array(true));
 
 						return [
 							'success' => true,
