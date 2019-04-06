1df5e8525a75ece50d964a6ea310a2809797037e
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-04-06 19:11:41 +0300

    Once again

--- src/Utils/Auth.php
+++ src/Utils/Auth.php
@@ -306,7 +306,7 @@ class Auth {
 				$user = new User();
 
 				foreach ($users as $users_array => $value) {
-					$user->user_send_verify_email($users_array['email'], $users_array['id'], $users_array['username']);	
+					$user->user_send_verify_email($value['email'], $value['id'], $value['username']);	
 				}
 
 				return [
