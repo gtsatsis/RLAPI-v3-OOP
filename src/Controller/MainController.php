e9de800fe3e49ce971090636c7cefb6c91f461f9
George Tsatsis <fafafa609@gmail.com>
2019-04-20 18:27:59 +0000

    Apply fixes from StyleCI

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,107 +1,100 @@
 <?php
+
 namespace App\Controller;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-use App\Models\User;
-use App\Models\Apikeys;
 use App\Models\Domains;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController {
-
-	/**
-     * Matches / exactly
+class MainController extends AbstractController
+{
+    /**
+     * Matches / exactly.
      *
      * @Route("/", name="index_page")
      */
+    public function index_page()
+    {
+        return new Response('You have reached the RLAPI v3.0 Index Page');
+    }
 
-	public function index_page(){
-		return new Response("You have reached the RLAPI v3.0 Index Page");
-	}
-
-	/**
-     * Matches /upload exactly
+    /**
+     * Matches /upload exactly.
      *
      * @Route("/upload", name="upload_index_page")
      */
+    public function upload_index_page()
+    {
+        return new Response('Upload Index');
+    }
 
-	public function upload_index_page(){
-		return new Response("Upload Index");
-	}
-
-	/**
-     * Matches /status exactly
+    /**
+     * Matches /status exactly.
      *
      * @Route("/status", name="status_page")
      */
+    public function status_page()
+    {
+        $response = new Response(json_encode([
+            'success' => 'true',
+            'code'    => 200,
+        ]));
 
-	public function status_page(){
-		$response = new Response(json_encode([
-			'success' => 'true',
-			'code' => 200
-		]));
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
 
-		return $response;
-	}
-
-	/**
-     * Matches /api exactly
+    /**
+     * Matches /api exactly.
      *
      * @Route("/api", name="api_endpoint_deprecated")
      */
+    public function api_endpoint_deprecated()
+    {
+        $response = new Response(json_encode([
+            'message' => 'api_endpoint_deprecated',
+        ]));
 
-	public function api_endpoint_deprecated(){
-		$response = new Response(json_encode([
-			'message' => 'api_endpoint_deprecated'
-		]));
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
 
-		return $response;
-	}
-
-	/**
-     * Matches /domains exactly
+    /**
+     * Matches /domains exactly.
      *
      * @Route("/domains", name="domains")
      */
+    public function domains()
+    {
+        $response = new Response(json_encode([
+            'message' => 'check_domains/list',
+        ]));
 
-	public function domains(){
-		$response = new Response(json_encode([
-			'message' => 'check_domains/list'
-		]));
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
 
-		return $response;
-	}
-
-	/**
-     * Matches /domains/list exactly
+    /**
+     * Matches /domains/list exactly.
      *
      * @Route("/domains/list", name="domains_list")
      */
+    public function domains_list()
+    {
+        $domains = new Domains();
 
-	public function domains_list(){
-		$domains = new Domains();
+        $list_domains = $domains->list_domains();
 
-		$list_domains = $domains->list_domains();
+        $response = new Response(json_encode($list_domains));
 
-		$response = new Response(json_encode($list_domains));
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->headers->set('Content-Type', 'application/json');
-
-		return $response;
-	}
+        return $response;
+    }
 }
-?>
--- src/Models/Domains.php
+++ src/Models/Domains.php
@@ -1,39 +1,36 @@
 <?php
-namespace App\Models;
-require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
-use Symfony\Component\Dotenv\Dotenv;
+namespace App\Models;
+
+require_once __DIR__.'/../../vendor/autoload.php';
 
 use App\Utils\Auth;
-use App\Utils\Mailer;
 use App\Utils\SqreenLib;
-use App\Utils\Getters;
+use Symfony\Component\Dotenv\Dotenv;
 
-class Domains {
+class Domains
+{
+    private $dbconn;
+    private $authentication;
+    private $sqreen;
 
-	private $dbconn;
-	private $authentication;
-	private $sqreen;
+    public function __construct()
+    {
 
-	public function __construct(){
+        /* Load the env file */
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
 
-		/* Load the env file */
-		$dotenv = new Dotenv();
-		$dotenv->load(__DIR__.'/../../.env');
+        /* Connect to database */
+        $this->dbconn = pg_connect('host='.getenv('DB_HOST').' port=5432 dbname='.getenv('DB_NAME').' user='.getenv('DB_USERNAME').' password='.getenv('DB_PASSWORD'));
 
-		/* Connect to database */
-		$this->dbconn = pg_connect("host=" . getenv('DB_HOST') . " port=5432 dbname=" . getenv('DB_NAME') . " user=" . getenv('DB_USERNAME') . " password=" . getenv('DB_PASSWORD'));
+        $this->authentication = new Auth();
 
-		$this->authentication = new Auth();
+        $this->sqreen = new SqreenLib();
+    }
 
-		$this->sqreen = new SqreenLib();
-
-	}
-
-	public function list_domains(){
-		return pg_fetch_all(pg_query($this->dbconn, "SELECT * FROM domains"));
-	}
+    public function list_domains()
+    {
+        return pg_fetch_all(pg_query($this->dbconn, 'SELECT * FROM domains'));
+    }
 }
-?>
