222e4088ba95471f252efb5cc0fc7b2f263b6e56
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-04-20 13:10:03 +0300

    Hide token from currently unauthenticated endpoint

--- src/Models/User.php
+++ src/Models/User.php
@@ -546,7 +546,7 @@ class User {
 
 	public function get_user_uploads(string $user_id){
 
-		pg_prepare($this->dbconn, "get_all_uploads_by_user", "SELECT * FROM files WHERE user_id = $1");
+		pg_prepare($this->dbconn, "get_all_uploads_by_user", "SELECT filename, originalfilename, timestamp, user_id, md5, sha1, deleted FROM files WHERE user_id = $1");
 		$execute_prepared_statement = pg_execute($this->dbconn, "get_all_uploads_by_user", array($user_id));
 
 		return pg_fetch_all($execute_prepared_statement);
