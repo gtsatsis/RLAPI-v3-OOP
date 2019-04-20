83f5c7292fe4d7480e533bf2e2bbd1f992c65600
George Tsatsis <fafafa609@gmail.com>
2019-04-20 18:24:55 +0000

    Apply fixes from StyleCI

--- src/Models/Buckets.php
+++ src/Models/Buckets.php
@@ -1,197 +1,150 @@
 <?php
+
 namespace App\Models;
-require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
-use \Aws\S3\S3Client;
-use \Aws\S3\AwsException;
-use Symfony\Component\Dotenv\Dotenv;
+require_once __DIR__.'/../../vendor/autoload.php';
+
 use App\Utils\Auth;
+use Symfony\Component\Dotenv\Dotenv;
 
-class Buckets {
+class Buckets
+{
+    private $dbconn;
+    private $s3;
 
-	private $dbconn;
-	private $s3;
+    public function __construct()
+    {
 
-	public function __construct(){
+        /* Load the env file */
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
 
-		/* Load the env file */
-		$dotenv = new Dotenv();
-		$dotenv->load(__DIR__.'/../../.env');
+        /* Connect to database */
+        $this->dbconn = pg_connect('host='.getenv('DB_HOST').' port=5432 dbname='.getenv('DB_NAME').' user='.getenv('DB_USERNAME').' password='.getenv('DB_PASSWORD'));
 
-		/* Connect to database */
-		$this->dbconn = pg_connect("host=" . getenv('DB_HOST') . " port=5432 dbname=" . getenv('DB_NAME') . " user=" . getenv('DB_USERNAME') . " password=" . getenv('DB_PASSWORD'));
+        $this->authentication = new Auth();
 
-		$this->authentication = new Auth();
+        $this->s3 = new \Aws\S3\S3Client(
+            [
+                'version'                 => 'latest', // Latest S3 version
+                'region'                  => 'us-east-1', // The service's region
+                'endpoint'                => getenv('S3_ENDPOINT'), // API to point to
+                'credentials'             => new \Aws\Credentials\Credentials(getenv('S3_API_KEY'), getenv('S3_API_SECRET')), // Credentials
+                'use_path_style_endpoint' => true, // Minio Compatible (https://minio.io)
+            ]
+        );
+    }
 
-		$this->s3 = new \Aws\S3\S3Client(
-			[
-				'version' => 'latest', // Latest S3 version
-				'region'  => 'us-east-1', // The service's region
-				'endpoint' => getenv('S3_ENDPOINT'), // API to point to
-				'credentials' => new \Aws\Credentials\Credentials(getenv('S3_API_KEY'), getenv('S3_API_SECRET')), // Credentials
-				'use_path_style_endpoint' => true // Minio Compatible (https://minio.io)
-        	]
-    	);
+    /* Begin Create Bucket Function */
 
-	}
+    public function create_new_user_bucket(string $user_id, string $bucket_name, string $password, string $allocated_domain = null)
+    {
+        if ($this->authentication->validate_password($user_id, $password)) {
+            if ($this->authentication->bucket_allowance($user_id)) {
+                $create_bucket = $this->s3->createBucket(['ACL' => 'public-read', 'Bucket' => $bucket_name, 'CreateBucketConfiguration' => ['LocationConstraint' => 'us-east-1']]);
 
+                if ($create_bucket != null) {
+                    pg_prepare($this->dbconn, 'insert_bucket', 'INSERT INTO buckets (user_id, bucket_name, allocated_domain) VALUES ($1, $2, $3)');
+                    $execute_prepared_statement = pg_execute($this->dbconn, 'insert_bucket', [$user_id, $bucket_name, $allocated_domain]);
 
-	/* Begin Create Bucket Function */
+                    if ($execute_prepared_statement) {
+                        return [
+                                'success' => true,
+                                'bucket'  => [
+                                    'location' => $create_bucket->get('Location'),
+                                ],
+                            ];
+                    } else {
+                        throw new Exception('Error Processing create_new_user_bucket Request: SQL');
+                    }
+                } else {
+                    throw new Exception('Error Processing create_new_user_bucket Request');
+                }
+            } else {
+                return [
+                    'success'       => false,
+                    'error_code'    => 1122,
+                    'error_message' => 'private_bucket_allowance_reached',
+                ];
+            }
+        } else {
+            return [
+                'success'       => false,
+                'error_code'    => 1002,
+                'error_message' => 'invalid_user_id_or_password',
+            ];
+        }
+    }
 
-	public function create_new_user_bucket(string $user_id, string $bucket_name, string $password, string $allocated_domain=null){
+    /* End Create Bucket Function */
 
-		if($this->authentication->validate_password($user_id, $password)){
+    /* Begin Delete Bucket Function */
 
-			if($this->authentication->bucket_allowance($user_id)){
+    public function delete_user_bucket(string $user_id, string $bucket_name, string $password, string $allocated_domain = null)
+    {
+        if ($this->authentication->validate_password($user_id, $password)) {
+            if ($this->authentication->owns_bucket($user_id, $bucket_name)) {
+                $delete_bucket = $this->s3->deleteBucket(['Bucket' => $bucket_name]);
 
-					$create_bucket = $this->s3->createBucket(['ACL' => 'public-read','Bucket' => $bucket_name,'CreateBucketConfiguration' => ['LocationConstraint' => 'us-east-1']]);
+                if ($delete_bucket != null) {
+                    pg_prepare($this->dbconn, 'delete_bucket', 'DELETE FROM buckets WHERE bucket_name = $1');
+                    $execute_prepared_statement = pg_execute($this->dbconn, 'delete_bucket', [$bucket_name]);
 
-					if($create_bucket != null){
+                    if ($execute_prepared_statement) {
+                        return [
+                            'success' => true,
+                        ];
+                    } else {
+                        throw new Exception('Error Processing delete_bucket Request: sql');
+                    }
+                }
+            } else {
+                return [
+                    'success'       => false,
+                    'error_code'    => 1100,
+                    'error_message' => 'unauthorized_not_owner',
+                ];
+            }
+        } else {
+            return [
+                'success'       => false,
+                'error_code'    => 1002,
+                'error_message' => 'invalid_user_id_or_password',
+            ];
+        }
+    }
 
-						pg_prepare($this->dbconn, "insert_bucket", "INSERT INTO buckets (user_id, bucket_name, allocated_domain) VALUES ($1, $2, $3)");
-						$execute_prepared_statement = pg_execute($this->dbconn, "insert_bucket", array($user_id, $bucket_name, $allocated_domain));
-						
-						if($execute_prepared_statement){
+    public function assign_domain_to_bucket(string $user_id, string $password, string $bucket_name, string $domain)
+    {
+        if ($this->authentication->validate_password($user_id, $password)) {
+            if ($this->authentication->owns_bucket($user_id, $bucket_name)) {
+                pg_prepare($this->dbconn, 'assign_domain_to_bucket', 'UPDATE buckets SET allocated_domain = $1 WHERE bucket = $2');
+                $execute_prepared_statement = pg_execute($this->dbconn, 'assign_domain_to_bucket', [$domain, $bucket_name]);
 
-							return [
-								'success' => true,
-								'bucket' => [
-									'location' => $create_bucket->get('Location')
-								]
-							];
-
-						}else{
-
-							throw new Exception("Error Processing create_new_user_bucket Request: SQL");
-							
-						}
-
-					}else{
-
-						throw new Exception("Error Processing create_new_user_bucket Request");
-						
-					}
-
-			}else{
-
-				return [
-					'success' => false,
-					'error_code' => 1122,
-					'error_message' => 'private_bucket_allowance_reached'
-				];
-
-			}
-		}else{
-
-			return [
-				'success' => false,
-				'error_code' => 1002,
-				'error_message' => 'invalid_user_id_or_password'
-			];
-
-		}
-
-	}
-
-	/* End Create Bucket Function */
-
-	/* Begin Delete Bucket Function */
-
-	public function delete_user_bucket(string $user_id, string $bucket_name, string $password, string $allocated_domain=null){
-
-		if($this->authentication->validate_password($user_id, $password)){
-
-			if($this->authentication->owns_bucket($user_id, $bucket_name)){
-				$delete_bucket = $this->s3->deleteBucket(['Bucket' => $bucket_name]);
-
-				if($delete_bucket != null){
-
-					pg_prepare($this->dbconn, "delete_bucket", "DELETE FROM buckets WHERE bucket_name = $1");
-					$execute_prepared_statement = pg_execute($this->dbconn, "delete_bucket", array($bucket_name));
-
-					if($execute_prepared_statement){	
-					
-						return [
-							'success' => true
-						];
-					
-					}else{
-
-						throw new Exception("Error Processing delete_bucket Request: sql");
-						
-					}
-
-				}
-
-			}else{
-
-				return [
-					'success' => false,
-					'error_code' => 1100,
-					'error_message' => 'unauthorized_not_owner'
-				];
-
-			}
-			
-		}else{
-
-			return [
-				'success' => false,
-				'error_code' => 1002,
-				'error_message' => 'invalid_user_id_or_password'
-			];
-
-		}
-
-	}
-
-	public function assign_domain_to_bucket(string $user_id, string $password, string $bucket_name, string $domain){
-
-		if($this->authentication->validate_password($user_id, $password)){
-
-			if($this->authentication->owns_bucket($user_id, $bucket_name)){
-
-				pg_prepare($this->dbconn, "assign_domain_to_bucket", "UPDATE buckets SET allocated_domain = $1 WHERE bucket = $2");
-				$execute_prepared_statement = pg_execute($this->dbconn, "assign_domain_to_bucket", array($domain, $bucket_name));
-
-				if($execute_prepared_statement){
-
-					return [
-						'success' => true,
-						'bucket' => [
-							$bucket => [
-								'domain' => $domain
-							]
-						]
-					];
-
-				}else{
-
-				}
-			
-			}else{
-
-				return [
-					'success' => false,
-					'error_code' => 1100,
-					'error_message' => 'unauthorized_not_owner'
-				];
-
-			}
-
-		}else{
-
-			return [
-				'success' => false,
-				'error_code' => 1002,
-				'error_message' => 'invalid_user_id_or_password'
-			];
-
-		}
-
-	}
-
+                if ($execute_prepared_statement) {
+                    return [
+                        'success' => true,
+                        'bucket'  => [
+                            $bucket => [
+                                'domain' => $domain,
+                            ],
+                        ],
+                    ];
+                } else {
+                }
+            } else {
+                return [
+                    'success'       => false,
+                    'error_code'    => 1100,
+                    'error_message' => 'unauthorized_not_owner',
+                ];
+            }
+        } else {
+            return [
+                'success'       => false,
+                'error_code'    => 1002,
+                'error_message' => 'invalid_user_id_or_password',
+            ];
+        }
+    }
 }
-?>
--- src/Utils/FileUtils.php
+++ src/Utils/FileUtils.php
@@ -1,61 +1,62 @@
 <?php
+
 namespace App\Utils;
 
-use Symfony\Component\Dotenv\Dotenv;
 use App\Models\User;
+use Symfony\Component\Dotenv\Dotenv;
 
-class FileUtils{
+class FileUtils
+{
+    private $dbconn;
 
-	private $dbconn;
-	
-	public function __construct(){
-		/* Load the env file */
-		$dotenv = new Dotenv();
-		$dotenv->load(__DIR__.'/../../.env');
-		/* Connect to database */
-		$this->dbconn = pg_connect("host=" . getenv('DB_HOST') . " port=5432 dbname=" . getenv('DB_NAME') . " user=" . getenv('DB_USERNAME') . " password=" . getenv('DB_PASSWORD'));
-	}
+    public function __construct()
+    {
+        /* Load the env file */
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
+        /* Connect to database */
+        $this->dbconn = pg_connect('host='.getenv('DB_HOST').' port=5432 dbname='.getenv('DB_NAME').' user='.getenv('DB_USERNAME').' password='.getenv('DB_PASSWORD'));
+    }
 
-	public function generateFileName($extension){
+    public function generateFileName($extension)
+    {
 
-    	// Generate a random name
-    	$fileName = substr(str_shuffle(str_repeat(getenv('FILENAME_DICTIONARY'), getenv('FILENAME_LENGTH'))), 0, getenv('FILENAME_LENGTH'));
-    	
-    	// Add file extension
-    	$fileName .= "." . $extension;
-    	return $fileName;
-	}
+        // Generate a random name
+        $fileName = substr(str_shuffle(str_repeat(getenv('FILENAME_DICTIONARY'), getenv('FILENAME_LENGTH'))), 0, getenv('FILENAME_LENGTH'));
 
-	public function isUnique($filename, $s3Endpoint){
+        // Add file extension
+        $fileName .= '.'.$extension;
 
-		$headers = get_headers($s3Endpoint . $filename);
-		
-		if (substr($headers[0], 9, 3) == "404") {
-		
-			return true;
-		
-    	}
-		
-		return false;
-	}
+        return $fileName;
+    }
 
-	public function log_object($api_key, $file_name, $file_original_name, $file_md5_hash, $file_sha1_hash){
-		$users = new User();
+    public function isUnique($filename, $s3Endpoint)
+    {
+        $headers = get_headers($s3Endpoint.$filename);
 
-		$user_id = $users->get_user_by_api_key($api_key);
+        if (substr($headers[0], 9, 3) == '404') {
+            return true;
+        }
 
-		$user_id = $user_id['id'];
-		if(!empty($user_id)){
-			$prepareStatement = pg_prepare($this->dbconn, "log_object", "INSERT INTO files VALUES ($1, $2, $3, $4, $5, $6, $7)");
-			$executePreparedStatement = pg_execute($this->dbconn, "log_object", array($file_name, $file_original_name, time(), $user_id, $api_key, $file_md5_hash, $file_sha1_hash));
+        return false;
+    }
 
-			if($executePreparedStatement){
-				return true;
-			}else{
-				throw new \Exception('Something went wrong while inserting a file into the database.');
-			}
-		}
-	}
+    public function log_object($api_key, $file_name, $file_original_name, $file_md5_hash, $file_sha1_hash)
+    {
+        $users = new User();
+
+        $user_id = $users->get_user_by_api_key($api_key);
+
+        $user_id = $user_id['id'];
+        if (!empty($user_id)) {
+            $prepareStatement = pg_prepare($this->dbconn, 'log_object', 'INSERT INTO files VALUES ($1, $2, $3, $4, $5, $6, $7)');
+            $executePreparedStatement = pg_execute($this->dbconn, 'log_object', [$file_name, $file_original_name, time(), $user_id, $api_key, $file_md5_hash, $file_sha1_hash]);
+
+            if ($executePreparedStatement) {
+                return true;
+            } else {
+                throw new \Exception('Something went wrong while inserting a file into the database.');
+            }
+        }
+    }
 }
-
-?>
