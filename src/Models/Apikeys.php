b626eceb3b017791c855bee9f61c7c2befa6960c
George Tsatsis <fafafa609@gmail.com>
2019-05-05 11:18:35 +0000

    Apply fixes from StyleCI

--- src/Models/Apikeys.php
+++ src/Models/Apikeys.php
@@ -234,7 +234,8 @@ class Apikeys
         }
     }
 
-    public function generate_sharex_config($api_key, $domain){
+    public function generate_sharex_config($api_key, $domain)
+    {
         return [
             'Version' => '12.4.1',
             'Name' => 'ratelimited.me - Free API: '.$api_key,
@@ -242,11 +243,11 @@ class Apikeys
             'RequestMethod' => 'POST',
             'RequestURL' => 'https://api.ratelimited.me/upload/pomf',
             'Parameters' => [
-                'key' => $api_key
+                'key' => $api_key,
             ],
             'Body' => 'MultipartFormData',
             'FileFormName' => 'files[]',
-            'URL' => $domain.'$json:files[0].url$'
+            'URL' => $domain.'$json:files[0].url$',
         ];
     }
 }
