9ed6428399d32801217fdcad073e38d879716b98
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-06-08 04:55:48 +0300

    Finish the stats endpoint

--- src/Models/Stats.php
+++ src/Models/Stats.php
@@ -33,15 +33,38 @@ class Stats
     public function getStats()
     {
         $active_users = pg_query($this->dbconn, 'SELECT COUNT(*) FROM users WHERE verified = true AND is_blocked IS NOT true');
+        $active_users = pg_fetch_array($active_users);
+
+        $upgraded_users = pg_query($this->dbconn, 'SELECT COUNT(*) FROM users WHERE tier NOT IN (\'free\')');
+        $upgraded_users = pg_fetch_array($upgraded_users);
+
         $total_users = pg_query($this->dbconn, 'SELECT COUNT(*) FROM users');
-        //$upgraded_users = pg_query($this->dbconn, 'SELECT COUNT(*) FROM users WHERE tier NOT IN (\'free\')');
+        $total_users = pg_fetch_array($total_users);
 
         $total_files = pg_query($this->dbconn, 'SELECT COUNT(*) FROM files');
+        $total_files = pg_fetch_array($total_files);
+
+        $active_promos = pg_query($this->dbconn, 'SELECT COUNT(*) FROM promo_codes WHERE expired = false');
+        $active_promos = pg_fetch_array($active_promos);
+
+        $total_promos = pg_query($this->dbconn, 'SELECT COUNT(*) FROM promo_codes');
+        $total_promos = pg_fetch_array($total_promos);
+
+        $total_domains = pg_query($this->dbconn, 'SELECT COUNT(*) FROM domains');
+        $total_domains = pg_fetch_array($total_domains);
 
         $stats_array = [
-            'active_users' => $active_users,
-            'total_users' => $total_users,
-            'files' => $total_files,
+            'users' => [
+                'active' => (int)$active_users[0],
+                'upgraded' => (int)$upgraded_users[0],
+                'total' => (int)$total_users[0],
+            ],
+            'files' => (int)$total_files[0],
+            'promos' => [
+                'active' => (int)$active_promos[0],
+                'total' => (int)$total_promos[0],
+            ],
+            'domains' => (int)$total_domains[0],
         ];
 
         return $stats_array;
