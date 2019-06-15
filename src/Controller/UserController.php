34f131ea0e85ddc67a0e9fdbbd9c66b53dc4567d
George Tsatsis <fafafa609@gmail.com>
2019-06-15 09:36:39 +0000

    Apply fixes from StyleCI

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -330,27 +330,30 @@ class UserController extends AbstractController
 
     /**
      * Matches /users/get_user_id exactly.
-     * 
+     *
      * @Route("/users/get_user_id", name="get_user_id")
      */
     public function get_user_id(Request $request)
     {
         $auth = new Auth();
         $getter = new Getters();
-         if($request->query->has('key')){
-            if($auth->isValidUUID($request->query->get('key'))){
+        if ($request->query->has('key')) {
+            if ($auth->isValidUUID($request->query->get('key'))) {
                 $get_user_id = $getter->get_user_by_api_key($request->query->get('key'));
                 $response = new Response(json_encode(['success' => true, 'user_id' => $get_user_id['user_id']]));
                 $response->headers->set('Content-Type', 'application/json');
+
                 return $response;
-            }else{
+            } else {
                 $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Invalid API key']]));
                 $response->headers->set('Content-Type', 'application/json');
+
                 return $response;
             }
-        }else{
+        } else {
             $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Missing API key']]));
             $response->headers->set('Content-Type', 'application/json');
+
             return $response;
         }
     }
