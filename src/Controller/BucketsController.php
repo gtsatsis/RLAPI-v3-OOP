4bb7e08f5590e9a71c66b4d3a4b3e29e993f863a
George Tsatsis <fafafa609@gmail.com>
2019-06-17 14:16:23 +0000

    Apply fixes from StyleCI

--- src/Controller/BucketsController.php
+++ src/Controller/BucketsController.php
@@ -86,40 +86,40 @@ class BucketsController extends AbstractController
 
     /**
      * Matches /buckets/{bucket_id}/users/add.
-     * 
+     *
      * @Route("/buckets/{bucket_id}/users/add", name="add_user")
      */
     public function add_user(Request $request, $bucket_id)
     {
         if ($request->request->has('api_key')) {
             if ($this->authentication->isValidUUID($bucket_id) && $this->authentication->isValidUUID($request->request->get('api_key'))) {
-                if($this->buckets->bucket_exists($this->getter->getBucketNameFromID($bucket_id)) && $this->buckets->user_is_in_bucket($request->request->get('api_key'), $bucket_id)){
+                if ($this->buckets->bucket_exists($this->getter->getBucketNameFromID($bucket_id)) && $this->buckets->user_is_in_bucket($request->request->get('api_key'), $bucket_id)) {
                     $permissions = $this->buckets->get_permissions($request->request->get('api_key'), $bucket_id);
-                    if($permissions['rlapi.custom.bucket.user.add'] == true){
+                    if (true == $permissions['rlapi.custom.bucket.user.add']) {
                         $add_user = $this->buckets->add_user($request->request->get('username'), $bucket_id);
                         $response = new Response(json_encode($add_user));
                         $response->headers->set('Content-Type', 'application/json');
 
                         return $response;
-                    }else{
+                    } else {
                         $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Unauthorized']]));
                         $response->headers->set('Content-Type', 'application/json');
 
                         return $response;
                     }
-                }else{
+                } else {
                     $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Unauthorized']]));
                     $response->headers->set('Content-Type', 'application/json');
 
                     return $response;
                 }
-            }else{
+            } else {
                 $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Bucket ID and API key not in UUID format.']]));
                 $response->headers->set('Content-Type', 'application/json');
 
                 return $response;
             }
-        }else{
+        } else {
             $response = new Response(json_encode(['success' => false, 'error' => ['error_message' => 'Missing required parameters', 'required' => ['api_key']]]));
             $response->headers->set('Content-Type', 'application/json');
 
