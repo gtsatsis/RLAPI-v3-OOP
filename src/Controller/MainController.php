7a47808d785190fb1e4235ac2e1685de4d4e9ea3
George Tsatsis <fafafa609@gmail.com>
2019-07-21 20:07:36 +0000

    Apply fixes from StyleCI

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,56 +1,49 @@
 <?php
+
 namespace App\Controller;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use App\Models\User;
-use App\Models\Apikeys;
-
-class MainController extends AbstractController {
-
-	/**
-     * Matches / exactly
+class MainController extends AbstractController
+{
+    /**
+     * Matches / exactly.
      *
      * @Route("/", name="index_page")
      */
+    public function index_page()
+    {
+        return new Response('You have reached the RLAPI v3.0 Index Page');
+    }
 
-	public function index_page(){
-		return new Response("You have reached the RLAPI v3.0 Index Page");
-	}
-
-	/**
-     * Matches /upload exactly
+    /**
+     * Matches /upload exactly.
      *
      * @Route("/upload", name="upload_index_page")
      */
+    public function upload_index_page()
+    {
+        return new Response('Upload Index');
+    }
 
-	public function upload_index_page(){
-		return new Response("Upload Index");
-	}
-
-	/**
-     * Matches /status exactly
+    /**
+     * Matches /status exactly.
      *
      * @Route("/status", name="status_page")
      */
+    public function status_page()
+    {
+        $response = new Response(json_encode([
+            'success' => 'true',
+            'code' => 200,
+        ]));
 
-	public function status_page(){
-		$response = new Response(json_encode([
-			'success' => 'true',
-			'code' => 200
-		]));
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->headers->set('Content-Type', 'application/json');
-
-		return $response;
-	}
+        return $response;
+    }
 }
-?>
--- src/Utils/SqreenLib.php
+++ src/Utils/SqreenLib.php
@@ -1,60 +1,43 @@
 <?php
+
 namespace App\Utils;
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 use Symfony\Component\Dotenv\Dotenv;
 
-class SqreenLib {
+class SqreenLib
+{
+    public function __construct()
+    {
+        /* Load the env file */
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__.'/../../.env');
+    }
 
+    public function sqreen_auth_track($success, $identifier)
+    {
+        if (getenv('SQREEN_ENABLED')) {
+            \sqreen\auth_track($success, ['email' => $identifier]);
+        }
+    }
 
-	public function __construct(){
+    public function sqreen_signup_track($identifier)
+    {
+        if (getenv('SQREEN_ENABLED')) {
+            \sqreen\signup_track(['email' => $identifier]);
+        }
+    }
 
-		/* Load the env file */
-		$dotenv = new Dotenv();
-		$dotenv->load(__DIR__.'/../../.env');
+    public function sqreen_track_upload($identifier)
+    {
+        if (getenv('SQREEN_ENABLED')) {
+            \sqreen\track('app.ratelimited.rlapi.upload', ['properties' => ['user_id' => $identifier]]);
+        }
+    }
 
-	}
-
-	public function sqreen_auth_track($success, $identifier){
-
-		if(getenv('SQREEN_ENABLED')){
-
-			\sqreen\auth_track($success, ['email' => $identifier]);
-
-		}
-
-	}
-
-	public function sqreen_signup_track($identifier){
-
-		if(getenv('SQREEN_ENABLED')){
-
-			\sqreen\signup_track(['email' => $identifier]);
-			
-		}
-
-	}
-
-	public function sqreen_track_upload($identifier){
-
-		if(getenv('SQREEN_ENABLED')){
-
-			\sqreen\track('app.ratelimited.rlapi.upload', ['properties' => ['user_id' => $identifier]]);
-			
-		}
-
-	}
-
-	public function sqreen_track_password_reset(){
-		
-		if(getenv('SQREEN_ENABLED')){
-
-			\sqreen\track('app.reset_password_request');
-
-		}
-	
-	}
-
+    public function sqreen_track_password_reset()
+    {
+        if (getenv('SQREEN_ENABLED')) {
+            \sqreen\track('app.reset_password_request');
+        }
+    }
 }
-?>
