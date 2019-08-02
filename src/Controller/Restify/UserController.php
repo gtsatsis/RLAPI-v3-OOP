1d96d57b237a2932adf1dd1ada9ab071febb3a63
George Tsatsis <gtsatsis@staff.ratelimited.me>
2019-08-02 12:24:53 +0300

    remove debug

--- src/Controller/Restify/UserController.php
+++ src/Controller/Restify/UserController.php
@@ -42,7 +42,7 @@ class UserController extends AbstractController
                 return $response;
             }
         } else {
-            $response = new Response(json_encode(array('success' => false, 'error_code' => 1082, print_r($data))));
+            $response = new Response(json_encode(array('success' => false, 'error_code' => 1082)));
             $response->headers->set('Content-Type', 'application/json');
 
             return $response;
